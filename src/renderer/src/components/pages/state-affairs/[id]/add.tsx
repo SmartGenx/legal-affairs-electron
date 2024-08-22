c8deb22f419b7e190294cf11ab2d992ef5e8a2d7
H-7117 <hamod2131.a@gmail.com>
2024-08-22 09:31:17 +0300

    starting the form

--- src/renderer/src/components/pages/state-affairs/[id]/add.tsx
+++ src/renderer/src/components/pages/state-affairs/[id]/add.tsx
@@ -2,16 +2,14 @@ import { Button } from '../../../ui/button'
 import { ArrowRight } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Header from './add-components/header'
+import AddIssueForm from './add-components/addIssueForm'
 
 export default function AddIssus() {
   return (
-    <div className=" flex items-center text-2xl">
-      <Link to={'/state-affairs'}>
-        <Button className="w-30 bg-transparent text-[#3734a9] hover:bg-[#3734a9] hover:text-white rounded-2xl border-2 border-[#3734a9]">
-          <ArrowRight size={30} />
-        </Button>
-      </Link>
-      <h1 className="mr-2"> إضافة قضية</h1>
-    </div>
+    <>
+      <Header />
+      <AddIssueForm />
+    </>
   )
 }
